d84c73139889bbd79319b4bbc6b5a2ec1da44455
Francesco Gentile <francesco.gentile@unitn.it>
2026-06-03 19:26:05 +0200

    Replace website

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import './layout.css';
-	import { ModeWatcher } from 'mode-watcher';
-
-	let { children } = $props();
-</script>
-
-<ModeWatcher defaultMode="light" />
-{@render children()}
